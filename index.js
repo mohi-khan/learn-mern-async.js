9b48fa96d4addbbbbd117a36c0b9e3d8ffb167a6
mohi-khan <mohi.khan@gmail.com>
2022-08-30 16:32:30 +0600

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>This is a Sync Test</h1>`;
+
